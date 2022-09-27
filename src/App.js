c50c9aa5d212a34b1340a15f786218a4dc1417c2
meet178 <lathiyam886@gmail.com>
2022-09-27 11:18:42 +0530

    change in App component & Add react-redux module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
-// import ReactRouter from "./component/ReactRouter";
-import UseContax from "./component/useContax/useContax";
+import ReactRouter from "./component/ReactRouter";
+// import UseContax from "./component/useContax/useContax";
 
 function App() {
   return (
-//       <ReactRouter />
-      <UseContax />
+      <ReactRouter />
+      // <UseContax />
   );
 }
 
 export default App;
+  
